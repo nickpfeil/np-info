242673e7b8bc3d617c1b15d64111f3c85cd0f1be
Nicklaus Pfeil <nick.pfeil@gmail.com>
2018-07-18 15:21:46 -0400

    survey

--- src/main/java/com/techelevator/npgeek/SurveyController.java
+++ src/main/java/com/techelevator/npgeek/SurveyController.java
@@ -5,8 +5,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import com.techelevator.npgeek.model.ParkDao;
+import com.techelevator.npgeek.model.Survey;
 import com.techelevator.npgeek.model.SurveyDao;
 
 @Controller
@@ -29,5 +31,33 @@ public class SurveyController {
 		modelHolder.put("parks", surveyDao.topParks());
 		
 		return "/surveyResults";
+	}
+	
+	@RequestMapping(path="/survey", method=RequestMethod.GET)
+	public String survey() {
+		return "/survey";
+	}
+	
+	@RequestMapping(path="/survey", method=RequestMethod.POST)
+	public String survey(
+				@RequestParam String parkCode,
+				@RequestParam Long surveyId,
+				@RequestParam String emailAddress,
+				@RequestParam String state,
+				@RequestParam String activityLevel,
+				RedirectAttributes flashScope  //pass a flash scope variable into saveNewProject method
+			) {
+			Survey newSurvey = new Survey();
+			newSurvey.setParkCode(parkCode);
+			newSurvey.setSurveyId(surveyId);
+			newSurvey.setEmailAddress(emailAddress);
+			newSurvey.setState(state);
+			newSurvey.setActivityLevel(activityLevel);
+			
+			surveyDao.save(newSurvey);
+			
+			flashScope.addFlashAttribute("message", "New survey saved!");
+			
+			return "redirect:/surveyResults";
 	}
 }
